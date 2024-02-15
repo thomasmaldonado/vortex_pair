adfa43d45ccb3456d5a4757a79681a40553c48ae
Thomas Maldonado <maldonado@princeton.edu>
2024-02-15 11:41:11 -0500

    preparing first sweep

--- run.sh
+++ run.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 #conda activate
-python solver.py 0.5 0.7 1 20 20 8 epsilon
+#python solver.py 0.5 0.7 1 20 20 8 epsilon
 #python plotter.py epsilon
+
+for n in {5..10}; 
+do
+    sbatch run.slurm $n/10 $n
+done
