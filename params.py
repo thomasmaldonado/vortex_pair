4fd81a81c947750f2f8559e4c2d99925d2abcdba
Thomas Maldonado <maldonado@princeton.edu>
2024-03-14 15:14:32 +0100

    investigating lower tolerance

--- params.py
+++ params.py
@@ -1,14 +1,14 @@
 import numpy as np
 import sys
 
-num_Ks = 6
+num_Ks = 2
 num_As = 20
 
-tol = 1e-6
+tol = 1e-8
 max_iter = 30
 
 def K_func(K_idx, A_idx):
-    Ks = np.logspace(1, 6, num=num_Ks, base=2)
+    Ks = np.logspace(1, 4, num=num_Ks, base=2)
     return Ks[K_idx]
 
 def A_func(K_idx, A_idx):
